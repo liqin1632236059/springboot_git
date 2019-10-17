9ae861133838353f8b5154f6cdb1b5a3ecc5ecb7
liqin <liqin@163.com>
2019-10-17 11:15:05 +0700

    提交1111

--- src/main/java/com/accp/Demo.java
+++ src/main/java/com/accp/Demo.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class Demo {
 	public static void main(String[] args) throws Exception {
 		System.out.println("第一次提交sts版本");
-		System.out.println("第二次修改sts版本");
+		System.out.println("第二次修改sts版本111");
 	}
 
 }	
